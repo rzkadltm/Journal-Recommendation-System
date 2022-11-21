f457ad145015504f9d2e26dcd7c2bd068b0a9b21
rzkadltm <rzkadltm@gmail.com>
2022-11-21 20:05:13 +0700

    Integrasi Backend dan Model Success

---
+++ model.py
@@ -0,0 +1,25 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_distances
+#from sklearn.metrics.pairwise import cosine_similarity
+from nltk.corpus import stopwords
+import pandas as pd
+from nltk.tokenize import word_tokenize
+cv = CountVectorizer(stop_words='english', tokenizer = word_tokenize)
+
+data_frame = pd.read_csv('final.csv')
+
+data_frame["Title"]=data_frame["Title of the Article"].apply(lambda x: x)
+data_frame["Abstract"]=data_frame["Abstract of the article"].apply(lambda x: x)
+
+df = data_frame[['Title', 'Authors', 'Abstract']]
+
+def recommendations(search):
+    
+    keyword = cv.fit_transform(df.Title)
+    content = search
+    code = cv.transform([content])
+    dist = cosine_distances(code, keyword)
+    a = dist.argsort()[0, :10]
+    result = df.loc[a]
+    
+    return result
