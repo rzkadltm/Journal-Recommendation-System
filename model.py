23e85d65e59a371c8b8a45b3789535ee55de0231
rzkadltm <rzkadltm@gmail.com>
2022-11-30 07:36:40 +0700

    Final model, fixing UI, move unused html files

--- model.py
+++ model.py
@@ -1,24 +1,31 @@
 from sklearn.feature_extraction.text import CountVectorizer
-# from sklearn.metrics.pairwise import cosine_distances
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.corpus import stopwords
 import pandas as pd
 from nltk.tokenize import word_tokenize
+import string
 
-cv = CountVectorizer(stop_words='english', tokenizer = word_tokenize)
 data_frame = pd.read_csv('final.csv')
 
-data_frame["Title"]=data_frame["Title of the Article"].apply(lambda x: x)
-data_frame["Abstract"]=data_frame["Abstract of the article"].apply(lambda x: x)
-df = data_frame[['Title', 'Authors', 'Abstract']]
+data_frame["title"]=data_frame["Title of the Article"].apply(lambda x: x)
+data_frame["abstrak"]=data_frame["Abstract of the article"].apply(lambda x: x)
+df = data_frame[['title', 'Authors', 'abstrak']]
 
+df['title'] = df['title'].str.lower() # mengubah menjadi huruf kecil semua
+df['abstrak'] = df['abstrak'].str.lower() #df['Authors'] = df['Authors'].str.lower()
+df['title'] = df['title'].str.translate(str.maketrans('', '', string.punctuation)) # menghapus kata dari tanda baca
+df['abstrak'] = df['abstrak'].str.translate(str.maketrans('', '', string.punctuation)) #df['Authors'] = df['Authors'].str.translate(str.maketrans('', '', string.punctuation))
+df['title_abstrak'] = df[['title', 'abstrak']].agg(''.join, axis=1)
+
+cv = CountVectorizer(stop_words='english', tokenizer = word_tokenize)
 def recommendations(search):
     
-    keyword = cv.fit_transform(df.Title)
+    keywords = cv.fit_transform(df.title_abstrak)
     content = search
     code = cv.transform([content])
-    dist = cosine_similarity(code, keyword)
-    a = dist.argsort()[0, :10]
+    dist = cosine_similarity(code, keywords)
+    a = dist.argsort()[0,:-11:-1]
     result = df.loc[a]
+    result1 = result[['title', 'Authors', 'abstrak']]
     
-    return result
+    return result1
