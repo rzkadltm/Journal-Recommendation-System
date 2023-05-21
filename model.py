563291f49780d6c2e130fe94277bfb0784da2876
Razak Adli Tama <76839067+rzkadltm@users.noreply.github.com>
2023-05-21 23:47:17 +0700

    Update model.py

--- model.py
+++ model.py
@@ -9,6 +9,68 @@ nltk.download('punkt')
 import warnings
 warnings.filterwarnings("ignore")
 
+# Read the CSV file
+data_frame = pd.read_csv('final.csv')
+data_frame.tail()
+
+# Create columns for title and abstract
+data_frame["title"] = data_frame["Title of the Article"].apply(lambda x: x)
+data_frame["abstrak"] = data_frame["Abstract of the article"].apply(lambda x: x)
+
+# Select relevant columns
+data = data_frame[['title', 'Authors', 'abstrak']]
+
+# Combine title and abstract into a single column
+data['title_abstrak'] = data[['title', 'abstrak']].agg(''.join, axis=1)
+
+# Remove rows with missing values
+df = data.dropna()
+
+# Convert text to lowercase
+df['title'] = df['title'].str.lower()
+df['title_abstrak'] = df['title_abstrak'].str.lower()
+df['abstrak'] = df['abstrak'].str.lower()
+
+# Remove punctuation from text
+df['title'] = df['title'].str.translate(str.maketrans('', '', string.punctuation))
+df['title_abstrak'] = df['title_abstrak'].str.translate(str.maketrans('', '', string.punctuation))
+df['abstrak'] = df['abstrak'].str.translate(str.maketrans('', '', string.punctuation))
+
+# Initialize CountVectorizer with tokenizer
+cv = CountVectorizer(stop_words='english', tokenizer=word_tokenize)
+
+def recommendations(search):
+    # Transform text using CountVectorizer
+    keywords = cv.fit_transform(df.title_abstrak)
+    contents = search
+    content = contents.translate(str.maketrans('', '', string.punctuation))
+    content1 = content.lower()
+    code = cv.transform([content1])
+    
+    # Calculate cosine similarity
+    dist = cosine_similarity(code, keywords)
+    
+    # Get top 10 most similar articles
+    a = dist.argsort()[0, :-11:-1]
+    result = df.loc[a]
+    result1 = result[['title', 'Authors', 'abstrak']]
+    
+    return result1
+
+recommendations('Machine learning for engineering')
+
+'''
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from nltk.corpus import stopwords
+import pandas as pd
+from nltk.tokenize import word_tokenize
+import string
+import nltk
+nltk.download('punkt')
+import warnings
+warnings.filterwarnings("ignore")
+
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.corpus import stopwords
@@ -56,3 +118,4 @@ def recommendations(search):
     return result1
     
 recommendations('Machine learning for engineering')
+'''
